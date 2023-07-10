b81766340b20574098dc9f194f1d270af57ff1e3
Marcos Júnior <mjworks2020@outlook.com>
2023-07-10 19:01:14 -0300

    Aprendendo utilizar hook useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,45 @@
+import { useState } from "react";
 import "./App.css";
 import FinanceControl from "./components/FinanceControl/FinanceControl";
 import Header from "./components/Header/Header";
+import { Movement } from "./models/interfaces/Movement/Movement";
 
 function App() {
+  const [currentBalance, setCurrentBalance] = useState(0); // State de saldo atual
+  const [currentExpenses, setCurrentExpenses] = useState(0); // State de despesas atual
+  const [movementsItens, setMovementsItens] = useState<Array<Movement>>([]); // State de movimentações
+
+  const setNewMovement = (movement: Movement) => {
+    if (movement) {
+      setMovementsItens((prevMovements) => {
+        const movements = [...prevMovements];
+        movements.unshift({
+          name: movement.name,
+          value: movement.value,
+          type: movement.type,
+          id: Math.random().toString(),
+        });
+        return movements;
+      });
+
+      movement.type === "Input" &&
+        setCurrentBalance(
+          (prevBalance) => prevBalance + Number(movement.value)
+        );
+
+      if (movement.type === "Output") {
+        setCurrentExpenses(
+          (prevExpenses) => prevExpenses + Number(movement.value)
+        );
+
+        currentBalance > 0 &&
+          setCurrentBalance(
+            (prevBalance) => prevBalance - Number(movement.value)
+          );
+      }
+    }
+  };
+
   return (
     <div>
       <Header />
