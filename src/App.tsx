f52bb43ce8394b6fd4f9dc59b050110fd5d774c5
Marcos Júnior <mjworks2020@outlook.com>
2023-07-10 18:30:18 -0300

    Aprendendo Conceito de Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import FinanceControl from "./components/FinanceControl/FinanceControl";
 import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div>
       <Header />
+      <FinanceControl />
     </div>
   );
 }
---
+++ src/components/FinanceControl/FinanceControl.tsx
@@ -0,0 +1,12 @@
+import "./FinanceControl.css";
+
+const FinanceControl = () => {
+  return (
+      <div className="container_finances">
+          {/* BALANCE */}
+          {/* EXPENSE */}
+    </div>
+  );
+};
+
+export default FinanceControl;
