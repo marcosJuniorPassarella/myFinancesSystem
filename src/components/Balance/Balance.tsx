fb901844983eacf7278685f0bc63aee95c7f9a36
Marcos Júnior <mjworks2020@outlook.com>
2023-07-11 19:27:00 -0300

    Implementando componente de Balance - Parte 3

--- src/components/Balance/Balance.tsx
+++ src/components/Balance/Balance.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faDollar } from "@fortawesome/free-solid-svg-icons";
 import { BalanceProps } from "../../models/interfaces/BalanceProps/BalanceProps";
@@ -20,6 +20,36 @@ const Balance = ({ emitMovement, currentBalance }: BalanceProps) => {
     setInputValue("");
   };
 
+  const formSubmitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (inputName.trim().length === 0 || inputValue.trim().length === 0) {
+      setIsFormValid(false);
+      return;
+    }
+
+    hideInputForm();
+    emitMovement({
+      name: inputName,
+      value: inputValue,
+      type: "Input",
+    });
+  };
+
+  const handleInputNameForm = (event: React.FormEvent<HTMLInputElement>) => {
+    const eventTarget = event.currentTarget as HTMLInputElement;
+    const eventValue = eventTarget.value;
+    inputName.trim().length > 0 ? setIsFormValid(true) : setIsFormValid(false);
+    setInputName(eventValue);
+  };
+
+  const handleInputValueForm = (event: React.FormEvent<HTMLInputElement>) => {
+    const eventTarget = event.currentTarget as HTMLInputElement;
+    const eventValue = eventTarget.value;
+    inputValue.trim().length > 0 ? setIsFormValid(true) : setIsFormValid(false);
+    setInputValue(eventValue);
+  };
+
   return (
     <div>
       <div className="balance_container">
@@ -37,6 +67,39 @@ const Balance = ({ emitMovement, currentBalance }: BalanceProps) => {
               title="Entrada"
               priority="Input"
             />
+          )}
+
+          {renderInputForm && (
+            <form onSubmit={formSubmitHandler}>
+              <div
+                className={`input_form_container ${
+                  !isFormValid ? "invalid" : ""
+                }`}
+              >
+                <input
+                  type="text"
+                  placeholder="Nome"
+                  className="balance_input"
+                  value={inputName}
+                  onChange={handleInputNameForm}
+                />
+                <input
+                  type="text"
+                  placeholder="Valor"
+                  className="balance_input"
+                  value={inputValue}
+                  onChange={handleInputValueForm}
+                />
+              </div>
+              <div className="actions_form_buttons_container">
+                <Button
+                  title="Cancelar"
+                  priority="Output"
+                  action={hideInputForm}
+                />
+                <Button title="Adicionar" priority="Input" type="submit" />
+              </div>
+            </form>
           )}
         </div>
       </div>
