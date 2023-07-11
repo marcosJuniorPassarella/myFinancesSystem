4ec211ce1b8f6b6ce4e22c6030a863ecc77ecb33
Marcos Júnior <mjworks2020@outlook.com>
2023-07-11 20:30:25 -0300

    Implementando componente de Expenses - Parte 2

--- src/components/Expense/Expense.tsx
+++ src/components/Expense/Expense.tsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { ExpenseProps } from "../../models/interfaces/ExpenseProps/ExpenseProps";
 import "./Expense.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPercent } from "@fortawesome/free-solid-svg-icons";
+import Button from "../Button/Button";
 const Expense = ({
   emitMovement,
   currentExpenses,
@@ -22,6 +23,40 @@ const Expense = ({
     setInputValue("");
   };
 
+  const formSubmitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (inputName.trim().length === 0 || inputValue.trim().length === 0) {
+      setIsFormValid(false);
+      return;
+    }
+
+    if (currentBalance >= Number(inputValue)) {
+      hideInputForm();
+      emitMovement({
+        name: inputName,
+        value: inputValue,
+        type: "Output",
+      });
+    } else {
+      setIsFormValid(false);
+    }
+  };
+
+  const handleInputNameForm = (event: React.FormEvent<HTMLInputElement>) => {
+    const eventTarget = event.currentTarget as HTMLInputElement;
+    const eventValue = eventTarget.value;
+    inputName.trim().length > 0 ? setIsFormValid(true) : setIsFormValid(false);
+    setInputName(eventValue);
+  };
+
+  const handleInputValueForm = (event: React.FormEvent<HTMLInputElement>) => {
+    const eventTarget = event.currentTarget as HTMLInputElement;
+    const eventValue = eventTarget.value;
+    inputValue.trim().length > 0 ? setIsFormValid(true) : setIsFormValid(false);
+    setInputValue(eventValue);
+  };
+
   return (
     <div>
       <div className="expense_container">
@@ -30,6 +65,50 @@ const Expense = ({
             <FontAwesomeIcon icon={faPercent} color="#E43F4d" size="2x" />
             <h2>Despesas</h2>
           </header>
+
+          <h3> {currentExpenses > 0 ? currentExpenses : "R$ 0"} </h3>
+
+          {!renderInputForm && (
+            <Button
+              action={handleRenderInputForm}
+              title="Saída"
+              priority="Output"
+              disable={currentBalance === 0}
+            />
+          )}
+
+          {renderInputForm && (
+            <form onSubmit={formSubmitHandler}>
+              <div
+                className={`input_form_container ${
+                  !isFormValid ? "invalid" : ""
+                }`}
+              >
+                <input
+                  type="text"
+                  placeholder="Nome"
+                  className="expense_input"
+                  value={inputName}
+                  onChange={handleInputNameForm}
+                />
+                <input
+                  type="text"
+                  placeholder="Valor"
+                  className="expense_input"
+                  value={inputValue}
+                  onChange={handleInputValueForm}
+                />
+              </div>
+              <div className="actions_form_buttons_container">
+                <Button
+                  title="Cancelar"
+                  priority="Output"
+                  action={hideInputForm}
+                />
+                <Button type="submit" title="Adicionar" priority="Input" />
+              </div>
+            </form>
+          )}
         </div>
       </div>
     </div>
